59dc674c1b0ba7cf2dcd4c6acb0c365a54ed486e
Austin Fiedor <79776656+auddyfuture@users.noreply.github.com>
2021-10-18 20:36:32 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,20 +4,19 @@ import java.util.HashSet;
 
 public class Main {
 	public static void main(String [] args) {
-	double square = 0;
-	HashSet<Double> set = new HashSet<Double>();
-	for (int i = 122; i >= 0; i--) {
-		System.out.println(i);
-	}
-for(double i = 2; i < 101; i++) {
-	for(double k = 2; k < 101; k++) {
-		square = Math.pow(i, k);
-		set.add(square);
+		double square = 0;
+		HashSet<Double> set = new HashSet<Double>();
+			for (int i = 122; i >= 0; i--) {
+				System.out.println(i);
+			}
+			for(double i = 2; i < 101; i++) {
+				for(double k = 2; k < 101; k++) {
+					square = Math.pow(i, k);
+					set.add(square);
+			}
 		}
+		System.out.println(set.size());
 	}
-System.out.println(set.size());
-
-}
 }
 
 
